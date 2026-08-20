aa0c4bf1b5d39b2e2fc41b7b74b68fe38074031f
Ankit Jaiswal <jaisankit0911@gmail.com>
2026-08-20 18:47:02 +0530

    Query Parameter

--- main.py
+++ main.py
@@ -2,45 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# Query Parameters are used to filter the data from the database.
 
-@app.get("/")
-def home():
-    return {"message": "Hello, FastAPI!"}
-
-
-@app.get("/users")
-def get_users():
+@app.get("/products")
+def get_products(
+    category:str ="Electronics Items" , min_price : int = 0, max_price : int = 10000):
     return {
-        "users": [
-            {"id": 1, "name": "Ankit"},
-            {"id": 2, "name": "Rahul"},
-            {"id": 3, "name": "Priya"}
-        ]
+        "categoy": category,
+        "min_price": min_price,
+        "max_price": max_price          
     }
-
-@app.get("/about")
-def about():
-    return {
-        "app": "FastAPI Example",
-        "version": "1.0.0",
-        "description": "This is a simple FastAPI application."
-    }
-
-
-
-@app.get("/homePage")
-def homePage():
-    return {"message" : "Hello This is Ankit Jaiswal a Python FullStack Developer aand this is my fastapi project"} 
-
-@app.get("/user/{user_id}")
-def get_user(user_id:int):
-    users = {
-        1: {"id": 1, "name": "Ankit"},
-        2: {"id": 2, "name": "Rahul"},
-        3: {"id": 3, "name": "Priya"}
-    }
-    user = users.get(user_id)
-    if user:
-        return user
-    else:
-        return {"error": "User not found"}
