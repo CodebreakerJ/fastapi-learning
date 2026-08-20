bcddda1fa099dcc7d10cddd0d4db545125f64b66
Ankit Jaiswal <jaisankit0911@gmail.com>
2026-08-20 17:00:44 +0530

    Path Parameter

--- main.py
+++ main.py
@@ -2,6 +2,45 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def home():
-    return {"message": "Welcome to the FastAPI application!"}
+    return {"message": "Hello, FastAPI!"}
+
+
+@app.get("/users")
+def get_users():
+    return {
+        "users": [
+            {"id": 1, "name": "Ankit"},
+            {"id": 2, "name": "Rahul"},
+            {"id": 3, "name": "Priya"}
+        ]
+    }
+
+@app.get("/about")
+def about():
+    return {
+        "app": "FastAPI Example",
+        "version": "1.0.0",
+        "description": "This is a simple FastAPI application."
+    }
+
+
+
+@app.get("/homePage")
+def homePage():
+    return {"message" : "Hello This is Ankit Jaiswal a Python FullStack Developer aand this is my fastapi project"} 
+
+@app.get("/user/{user_id}")
+def get_user(user_id:int):
+    users = {
+        1: {"id": 1, "name": "Ankit"},
+        2: {"id": 2, "name": "Rahul"},
+        3: {"id": 3, "name": "Priya"}
+    }
+    user = users.get(user_id)
+    if user:
+        return user
+    else:
+        return {"error": "User not found"}
